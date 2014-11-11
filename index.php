a8eaaddac2e4ec97b491f57652cbd0ae1f3f0863
Davide Perozzi <triplexp@live.de>
2014-11-11 23:44:25 +0100

    updated php lib

--- index.php
+++ index.php
@@ -1,4 +1,22 @@
 <?php
+	define("ROOT_DIR", __DIR__);
+	define("FEWLINES_PHP", ROOT_DIR . '/php');
 
+	// Set include path for the php libs
+	set_include_path(implode(PATH_SEPARATOR, array(
+			FEWLINES_PHP, get_include_path()
+		)));
 
+	// Use autoload method from php
+	function __autoload($classPath)
+	{
+    	require_once $classPath . ".php";
+	}
+
+	// Inlcude the application ("bootstrap")
+	require_once "Fewlines/Application.php";
+
+	// Instantiate the application
+	$application = new \Fewlines\Application;
+	$application->run();
 ?>
