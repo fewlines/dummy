c2eb980bbfe01034258f1cc79034952eecb96094
Davide Perozzi <triplexp@live.de>
2014-11-12 21:40:15 +0100

    updated with autoloader

--- index.php
+++ index.php
@@ -1,22 +1,21 @@
 <?php
+	// Constants
 	define("ROOT_DIR", __DIR__);
 	define("FEWLINES_PHP", ROOT_DIR . '/php');
+	define("AL_FNC", '\Fewlines\Autoloader::loadClass');
 
 	// Set include path for the php libs
 	set_include_path(implode(PATH_SEPARATOR, array(
 			FEWLINES_PHP, get_include_path()
 		)));
 
-	// Use autoload method from php
-	function __autoload($classPath)
-	{
-    	require_once $classPath . ".php";
-	}
+	// Include the Autoloader
+	require_once "Fewlines/Autoloader.php";
 
 	// Inlcude the application ("bootstrap")
 	require_once "Fewlines/Application.php";
 
 	// Instantiate the application
-	$application = new \Fewlines\Application;
+	$application = new \Fewlines\Application(AL_FNC);
 	$application->run();
 ?>
