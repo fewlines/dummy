c87aef04952e9527483c66abd6d7c0df1c59809c
Davide Perozzi <triplexp@live.de>
2014-11-15 18:49:01 +0100

    udated php lib

--- index.php
+++ index.php
@@ -2,20 +2,21 @@
 	// Constants
 	define("ROOT_DIR", __DIR__);
 	define("FEWLINES_PHP", ROOT_DIR . '/php');
-	define("AL_FNC", '\Fewlines\Autoloader::loadClass');
+
+	define("URL_LAYOUT_ROUTE", "/view:index/action:index");
+
+	define("AL_FNC", '\Fewlines\Autoloader\Autoloader::loadClass');
 
 	// Set include path for the php libs
 	set_include_path(implode(PATH_SEPARATOR, array(
 			FEWLINES_PHP, get_include_path()
 		)));
 
-	// Include the Autoloader
-	require_once "Fewlines/Autoloader.php";
-
 	// Inlcude the application ("bootstrap")
-	require_once "Fewlines/Application.php";
-
+	require_once "Fewlines/Application/Application.php";
+echo "<pre>";
 	// Instantiate the application
-	$application = new \Fewlines\Application(AL_FNC);
+	$application = new \Fewlines\Application\Application(AL_FNC);
 	$application->run();
+echo "</pre>";
 ?>
