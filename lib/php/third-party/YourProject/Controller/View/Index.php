67a65528be49e6b7f608989c6d60cca46403b6c3
Davide Perozzi <triplexp@live.de>
2015-03-30 23:09:18 +0200

    updated view controller

--- lib/php/third-party/YourProject/Controller/View/Index.php
+++ lib/php/third-party/YourProject/Controller/View/Index.php
@@ -5,5 +5,8 @@ class Index extends \Fewlines\Controller\Template
 {
 	public function indexAction() {
 		echo "Action called from: " . __CLASS__;
+
+		// Assign version
+		$this->assign('version', $this->getConfig()->getElementByPath('application/version'));
 	}
 }
