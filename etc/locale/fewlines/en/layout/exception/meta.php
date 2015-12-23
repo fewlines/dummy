2fc18744b4ea95c6daebdb518ce26fa77b7cb180
Davide Perozzi <triplexp@live.de>
2015-12-23 17:55:18 +0100

    REMOVE all files (reason: outdated)

--- etc/locale/fewlines/en/layout/exception/meta.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return array(
-	'title' => 'An Error Occured',
-	'non-local-error' => 'Something went wrong!'
-);
--- etc/locale/fewlines/en/view/index/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return array(
-	'h1' => 'Default layout',
-	'h2' => 'This is the default index view',
-	'text' => 'Congratulations! You just set up your first application with fewlines'
-);
